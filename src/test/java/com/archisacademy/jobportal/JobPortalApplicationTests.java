2da1fa1ff42b80ee3742233015d9983436b832eb
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2024-05-13 17:16:39 +0300

    Initial commit

---
+++ src/test/java/com/archisacademy/jobportal/JobPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archisacademy.jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
