6d221996c4046d71351373657566ede615ae7e0e
bidoluyazılım <baranagtas1903@gmail.com>
2024-05-20 16:37:35 +0300

    debug

--- src/main/resources/db/migration/V2__update_user_table.sql
+++ src/main/resources/db/migration/V2__update_user_table.sql
@@ -1,4 +1,15 @@
 -- V2__update_users_table.sql
+DROP TABLE IF EXISTS "profiles";
+
+DROP SEQUENCE IF EXISTS profile_seq;
+
+CREATE SEQUENCE profile_seq INCREMENT BY 1 MINVALUE 0 MAXVALUE 2147483647 START WITH 1;
+
+CREATE TABLE  "public"."profiles"
+(
+    "id" INTEGER DEFAULT nextval('profile_seq') NOT NULL PRIMARY KEY,
+    "name" VARCHAR(255)
+);
 
 ALTER TABLE "public"."users"
     ADD COLUMN "uuid" VARCHAR(36),
@@ -6,5 +17,8 @@ ALTER TABLE "public"."users"
     ADD COLUMN "address" TEXT,
     ADD COLUMN "user_type" VARCHAR(50) NOT NULL,
     ADD COLUMN "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    ADD COLUMN "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
-    ADD COLUMN "p"
+    ADD COLUMN "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    ADD COLUMN "profile_id" INTEGER NOT NULL;
+
+ALTER TABLE "public"."users"
+    ADD CONSTRAINT "fk_profile_id" FOREIGN KEY (profile_id) REFERENCES "public"."profiles"(id);
