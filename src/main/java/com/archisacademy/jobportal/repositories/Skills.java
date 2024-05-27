5348f8eac0df724371706bd8ed19be9207404b28
furkanErenoglu <99097997+furkanErenoglu@users.noreply.github.com>
2024-05-27 16:06:18 +0300

    [DE-25] Created Skills repository

---
+++ src/main/java/com/archisacademy/jobportal/repositories/Skills.java
@@ -0,0 +1,6 @@
+package com.archisacademy.jobportal.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Skills extends JpaRepository<Skills, Long> {
+}
