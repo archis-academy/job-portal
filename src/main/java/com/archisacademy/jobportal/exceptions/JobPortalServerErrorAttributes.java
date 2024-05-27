f8029eef363c8d8eb3230cb7d6225879505f1d73
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2024-05-27 17:47:09 +0300

    [DE-2] - create attributes

---
+++ src/main/java/com/archisacademy/jobportal/exceptions/JobPortalServerErrorAttributes.java
@@ -0,0 +1,29 @@
+package com.archisacademy.jobportal.exceptions;
+
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.web.context.request.WebRequest;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JobPortalServerErrorAttributes extends DefaultErrorAttributes {
+
+    public JobPortalServerErrorAttributes(){
+        super();
+    }
+
+    @Override
+    public Map<String,Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options){
+        final Map<String, Object> defaultAttributes = super.getErrorAttributes(webRequest,options);
+
+        Map<String,Object> errorAttributes = new HashMap<>();
+        errorAttributes.put("status",defaultAttributes.get("status"));
+        errorAttributes.put("timstamp",defaultAttributes.get("timestamp"));
+        Object message = defaultAttributes.get("message");
+        message = "No message available".equals(message)? defaultAttributes.get("message"):message;
+        errorAttributes.put("message",message);
+        return errorAttributes;
+    }
+}
