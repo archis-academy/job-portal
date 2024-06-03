35bc55e9f424821720b925b30316e54aa0aca286
furkanErenoglu <99097997+furkanErenoglu@users.noreply.github.com>
2024-06-03 16:08:49 +0300

    [DE-29]- Created Skills Controller

---
+++ src/main/java/com/archisacademy/jobportal/controller/SkillsController.java
@@ -0,0 +1,50 @@
+package com.archisacademy.jobportal.controller;
+
+import com.archisacademy.jobportal.dto.SkillsDto;
+import com.archisacademy.jobportal.services.SkillsService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/skills")
+public class SkillsController {
+    private final SkillsService skillsService;
+
+    public SkillsController(SkillsService skillsService) {
+        this.skillsService = skillsService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createSkills(@RequestBody SkillsDto skillsDto, @RequestParam Long profileId){
+        return ResponseEntity.ok(skillsService.createSkill(skillsDto, profileId));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSkill(@PathVariable Long id) {
+        skillsService.deleteSkill(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateSkill(@PathVariable Long id, @RequestBody SkillsDto skillsDto) {
+        String message = skillsService.updateSkill(id, skillsDto);
+        return ResponseEntity.ok(message);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SkillsDto>> getAllSkills() {
+        List<SkillsDto> skillsDtos = skillsService.getAllSkills();
+        return ResponseEntity.ok(skillsDtos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SkillsDto> getSkillById(@PathVariable Long id) {
+        SkillsDto skillsDto = skillsService.getSkillById(id);
+        if (skillsDto == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(skillsDto);
+    }
+}
