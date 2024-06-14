0dd386edd20563804f70826d9cdfa83e727d35d4
Baran Agtas <baranagtas1903@gmail.com>
2024-06-14 16:35:37 +0300

    controller

--- src/main/java/com/archisacademy/jobportal/controller/EducationController.java
+++ src/main/java/com/archisacademy/jobportal/controller/EducationController.java
@@ -1,9 +1,9 @@
 package com.archisacademy.jobportal.controller;
 
-
 import com.archisacademy.jobportal.dto.EducationDto;
 import com.archisacademy.jobportal.services.EducationService;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,28 +20,28 @@ public class EducationController {
 
     @PostMapping
     public ResponseEntity<String> createEducation(@RequestBody EducationDto educationDto) {
-        return ResponseEntity.ok(educationService.createEducation(educationDto));
+        return new ResponseEntity<>(educationService.createEducation(educationDto), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteEducation(@PathVariable Long id) {
-        return ResponseEntity.ok(educationService.deleteEducation(id));
+        return new ResponseEntity<>(educationService.deleteEducation(id), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<String> updateEducation(@PathVariable Long id, @RequestBody EducationDto educationDto) {
-        return ResponseEntity.ok(educationService.updateEducation(id, educationDto));
+        return new ResponseEntity<>(educationService.updateEducation(id, educationDto), HttpStatus.OK);
     }
 
     @GetMapping
     public ResponseEntity<Page<EducationDto>> getAllEducations(
             @RequestParam(defaultValue = "0") int pageNo,
             @RequestParam(defaultValue = "10") int pageSize) {
-        return ResponseEntity.ok(educationService.getAllEducations(pageNo, pageSize));
+        return new ResponseEntity<>(educationService.getAllEducations(pageNo, pageSize), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<EducationDto> getEducationById(@PathVariable Long id) {
-        return ResponseEntity.ok(educationService.getEducationsById(id));
+        return new ResponseEntity<>(educationService.getEducationsById(id), HttpStatus.OK);
     }
 }
