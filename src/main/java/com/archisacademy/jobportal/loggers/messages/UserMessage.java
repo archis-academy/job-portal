4da57adeae2d4a0d21ed92ebef1fd7c455cf24fb
furkanErenoglu <99097997+furkanErenoglu@users.noreply.github.com>
2024-07-14 12:10:18 +0300

    [DE-28]- Update User Message

--- src/main/java/com/archisacademy/jobportal/loggers/messages/UserMessage.java
+++ src/main/java/com/archisacademy/jobportal/loggers/messages/UserMessage.java
@@ -6,7 +6,7 @@ public final class UserMessage {
         throw new UnsupportedOperationException("Utility class cannot be instantiated");
     }
 
-    public static final String USER_NOT_FOUND = "User not found with UUID: ";
+    public static final String USER_NOT_FOUND = "User not found with ID: ";
     public static final String USER_CREATED_SUCCESS = "User created successfully";
     public static final String USER_DELETED_SUCCESS = "User deleted successfully";
     public static final String USER_UPDATED_SUCCESS = "User updated successfully";
